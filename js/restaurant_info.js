cb7d95fef3e96b6189beda0402ca5aa0a4d8d093
natacha romano <romano.n@hotmail.com>
2018-11-10 15:21:06 -0200

    respinsible for large screen

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -149,18 +149,22 @@ fillReviewsHTML = (reviews = self.restaurant.reviews) => {
 createReviewHTML = (review) => {
   const li = document.createElement('li');
   const name = document.createElement('p');
+  name.classList.add("review-name");
   name.innerHTML = review.name;
   li.appendChild(name);
 
-  const date = document.createElement('p');
+  const date = document.createElement('span');
+  date.classList.add("review-date");
   date.innerHTML = review.date;
-  li.appendChild(date);
+  name.appendChild(date);
 
   const rating = document.createElement('p');
+  rating.classList.add("review-rating");
   rating.innerHTML = `Rating: ${review.rating}`;
   li.appendChild(rating);
 
   const comments = document.createElement('p');
+  comments.classList.add("review-comments");
   comments.innerHTML = review.comments;
   li.appendChild(comments);
 
